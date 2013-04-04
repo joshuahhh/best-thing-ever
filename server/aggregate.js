d461499d41efb8a83b44850f01a1cee20e07a2e1
joshuahhh <joshuah@mit.edu>
2013-04-04 15:23:13 -0700

    senselessly messing around with server aggregation

--- server/aggregate.js
+++ server/aggregate.js
@@ -1,32 +1,15 @@
-////////// Server only aggregation functions//////////
+////////// Server-only aggregation functions //////////
 
 
-update_score = function (comparison) {
-  if (comparison.choice===1) {
-    Contenders.update({_id: comparison.option1}, {$inc: {score: 1}});
-  }
-  else {
-    Contenders.update({_id: comparison.option2}, {$inc: {score: 1}});
-  }
-  console.log(Contenders);
+update_score = function (comp) {
+  var winnerID = (comp.choice == 1 ? comp.option1 : comp.option2);
+  Contenders.update({_id: winnerID}, {$inc: {score: 1}});
 };
 
 create_scores = function () {
-  var comparisons = Comparisons.find();
-  comparisons.forEach(update_score);
+  Comparisons.find({}).forEach(update_score);
 };
 
 reset_scores = function () {
-  var contenders = Contenders.find();
   Contenders.update({}, {$set: {score: 0}}, {multi: true});
 };
-
-
-
-/*
-  Each comparison is represented by a document in the Comparisons collection:
-    option1: contender id
-    option2: contender id
-    choice: int (1 or 2)
-    session: session id
-*/
